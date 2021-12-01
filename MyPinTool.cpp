9e43a038db07fb1ab23e95668a1216977d01771c
One946 <giuseppe.dagostino94@gmail.com>
2021-12-01 18:36:31 +0100

    Add files via upload

--- MyPinTool.cpp
+++ MyPinTool.cpp
@@ -28,7 +28,7 @@
 namespace W {
 #define _WINDOWS_H_PATH_ C:/Program Files/Windows Kits/10/Include/10.0.17763.0/um
 #include <Windows.h>
-//#include <ntdef.h>
+	//#include <ntdef.h>
 #include <ntstatus.h>
 }
 using namespace std;
@@ -36,14 +36,14 @@ using namespace std;
 //TYPEDEF
 //*******************************************************************
 enum {
-	VirtualQuery_INDEX=0,
+	VirtualQuery_INDEX = 0,
 	VirtualQueryEx_INDEX,
 	CoTaskMemAlloc_INDEX,
 	GlobalAlloc_INDEX,
 	HeapAlloc_INDEX,
 	LocalAlloc_INDEX,
 	malloc_INDEX,
-	new_INDEX, 
+	new_INDEX,
 	VirtualAlloc_INDEX
 };
 typedef struct mem_regions_t {
@@ -80,14 +80,27 @@ mem_regions mem_array[100]; //array in which i store valuable informations about
 int counter = 0; //counter for instructions
 int unkId = 0; //index for unknown regions in memory
 mem_map op_map[10000];
+// pointers for function results
 int* p2BuffVQ;
 int* p2BuffVQEx;
+ADDRINT CTMAlloc;
+ADDRINT GAlloc;
+ADDRINT HAlloc;
+ADDRINT LAlloc;
+ADDRINT mAlloc;
+ADDRINT VAlloc;
+
+static map<std::string, int> fMap;
 //*******************************************************************
 //FUNCTIONS
 //******************************************************************* 
 //function to record a write if falls within known mem_region
+
+/********************************************************************/
+/****************************ValidateMemory**********************************/
+/********************************************************************/
 BOOL  validateRead(VOID * ip, VOID * addr) {
-	bool found=1; // to use if then call i have to return 1 if i want to execute thencall
+	bool found = 1; // to use if then call i have to return 1 if i want to execute thencall
 	for (int i = 0; i < img_counter; i++) {
 		if ((int)addr < mem_array[i].high && (int)addr >= mem_array[i].low) {
 			found = 0; // to no use the then call i have to return 0
@@ -130,17 +143,17 @@ VOID RecordMemR(VOID * addr) {
 	}
 	if (!done) {
 		W::VirtualQuery((W::LPCVOID)addr, &memInfo, sizeof(memInfo));
-		if(img_counter<100){
-		mem_reg = (int)memInfo.BaseAddress + memInfo.RegionSize;
-		mem_array[img_counter].protection = memInfo.Protect;
-		mem_array[img_counter].id = img_counter;
-		mem_array[img_counter].high = mem_reg-1;
-		mem_array[img_counter].low = (int)memInfo.BaseAddress;
-		mem_array[img_counter].name = "Unknown";
-		mem_array[img_counter].protection = memInfo.Protect;
-		mem_array[img_counter].pagesType = memInfo.Type;
-		mem_array[img_counter].unloaded = 0;
-		img_counter++;
+		if (img_counter < 100) {
+			mem_reg = (int)memInfo.BaseAddress + memInfo.RegionSize;
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].id = img_counter;
+			mem_array[img_counter].high = mem_reg - 1;
+			mem_array[img_counter].low = (int)memInfo.BaseAddress;
+			mem_array[img_counter].name = "Unknown";
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].pagesType = memInfo.Type;
+			mem_array[img_counter].unloaded = 0;
+			img_counter++;
 		}
 	}
 }
@@ -152,7 +165,7 @@ VOID ValidateMemory(INS ins, VOID *v) {
 	{
 		if (INS_MemoryOperandIsRead(ins, memOp)) {
 			INS_InsertIfCall(
-				ins, IPOINT_BEFORE, 
+				ins, IPOINT_BEFORE,
 				(AFUNPTR)validateRead,
 				IARG_INST_PTR,
 				IARG_MEMORYOP_EA, memOp,
@@ -167,7 +180,9 @@ VOID ValidateMemory(INS ins, VOID *v) {
 		}
 	}
 }
-
+/********************************************************************/
+/****************************THREDS**********************************/
+/********************************************************************/
 VOID findStacks(CONTEXT *ctxt) {
 	TraceFile << "in findStacks\n";
 	int base;
@@ -183,7 +198,7 @@ VOID findStacks(CONTEXT *ctxt) {
 	max = (int)memInfo.RegionSize + (int)memInfo.BaseAddress - 1;
 	if (img_counter < 100) {// still not working, i have to figure out how to add regions once while checking the whole array
 		for (int i = 0; i < img_counter; i++) {
-			if ((int)currentSP < mem_array[i].high && (int)currentSP >= mem_array[i].low){
+			if ((int)currentSP < mem_array[i].high && (int)currentSP >= mem_array[i].low) {
 				TraceFile << "checking todo";
 				todo = 0;
 				index = i;
@@ -205,13 +220,14 @@ VOID findStacks(CONTEXT *ctxt) {
 			img_counter++;
 		}
 	}
-}	
-
+}
 VOID OnThreadStart(THREADID tid, CONTEXT *ctxt, INT32, VOID *) {
-	TraceFile << "in thread start \n"; 
+	TraceFile << "in thread start \n";
 	findStacks(ctxt);
-	}
-
+}
+/********************************************************************/
+/************************Validate Virtual Query**********************/
+/********************************************************************/
 VOID ArgVQEx(char *name, ADDRINT arg0, ADDRINT arg1, ADDRINT arg2, ADDRINT arg3) {
 	int* lpbuffer = (int*)arg2;
 	p2BuffVQEx = lpbuffer;
@@ -224,15 +240,7 @@ VOID ArgVQ(char *name, ADDRINT arg0, ADDRINT arg1, ADDRINT arg2) { //lpbuffer of
 //function to retrive VirtualQuery return value	
 VOID VQAfter(ADDRINT ret, IMG img) {
 	W::MEMORY_BASIC_INFORMATION* result = (W::MEMORY_BASIC_INFORMATION *)p2BuffVQ;
-	for (int i = 0; i < 50; i++) {
-		if ((int)result->AllocationBase >= mem_array[i].low && (int)result->AllocationBase < mem_array[i].high) {
-			/*TraceFile << "\n spotted an address contained in a module";
-			TraceFile << "\nThe module is: " << mem_array[i].name;
-			TraceFile << "\n max address of the pages belonging to the image is: " << mem_array[i].high;
-			TraceFile << "\n base address of the pages belonging to the image is: " << mem_array[i].low;
-			TraceFile << "\n the id of the image is: " << mem_array[i].id;*/
-		}
-	}
+	TraceFile << "Return value of VirtualQuery: " << result->BaseAddress << " \n";
 }
 VOID VQExAfter(ADDRINT ret) {
 	W::MEMORY_BASIC_INFORMATION* result = (W::MEMORY_BASIC_INFORMATION *)p2BuffVQEx;
@@ -246,49 +254,268 @@ VOID VQExAfter(ADDRINT ret) {
 		}
 	}
 }
-VOID instrumentVQ(IMG img, VOID *v) {
-	//CoTaskMemAlloc  GlobalAlloc   HeapAlloc   LocalAlloc	malloc		new   VirtualAlloc
-	const char* name1 = "VirtualQuery";
-	const char* name2 = "VirtualQueryEx";
-	RTN rtn1 = RTN_FindByName(img, name1);
-	RTN rtn2 = RTN_FindByName(img, name2);
-	if (RTN_Valid(rtn1)) {
-		RTN_Open(rtn1);
-		//function to parse VirtualQuery arguments
-		RTN_InsertCall(rtn1, IPOINT_BEFORE, (AFUNPTR)ArgVQ,
-			IARG_ADDRINT, "VirtualQuery",
-			IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_FUNCARG_ENTRYPOINT_VALUE, 1, IARG_FUNCARG_ENTRYPOINT_VALUE, 2,
-			IARG_END);
-		//function to retrive VirtualQuery return value	
-		RTN_InsertCall(rtn1, IPOINT_AFTER, (AFUNPTR)VQAfter,
-			IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
-		RTN_Close(rtn1);
+
+/********************************************************************/
+/****************************HEAPS**********************************/
+/********************************************************************/
+VOID CTMAAfter(ADDRINT ret, IMG img) {
+	CTMAlloc =  ret;
+	int todo = 1;
+	W::MEMORY_BASIC_INFORMATION memInfo;
+	if (img_counter < 100) {// still not working, i have to figure out how to add regions once while checking the whole array
+		for (int i = 0; i < img_counter; i++) {
+			if ((int)ret < mem_array[i].high && (int)ret >= mem_array[i].low) {
+				todo = 0;
+				break;
+			}
+		}
+		if (todo) {
+			W::VirtualQuery((W::LPCVOID)ret, &memInfo, sizeof(memInfo));
+			int mem_reg = (int)memInfo.BaseAddress + memInfo.RegionSize;
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].id = img_counter;
+			mem_array[img_counter].high = mem_reg - 1;
+			mem_array[img_counter].low = (int)memInfo.BaseAddress;
+			mem_array[img_counter].name = "CoTaskMemAllocResult";
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].pagesType = memInfo.Type;
+			mem_array[img_counter].unloaded = 0;
+			img_counter++;
+		}
 	}
-	if (RTN_Valid(rtn2)) {// does not work properly need to reconfigure for VQEx using function for VQ
-		RTN_Open(rtn2);
-		//function to parse VirtualQueryEx arguments
-		RTN_InsertCall(rtn2, IPOINT_BEFORE, (AFUNPTR)ArgVQEx,
-			IARG_ADDRINT, "VirtualQueryEx",
-			IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_FUNCARG_ENTRYPOINT_VALUE, 1, IARG_FUNCARG_ENTRYPOINT_VALUE, 2,
-			IARG_END);
-		//function to retrive VirtualQuery return value	
-		RTN_InsertCall(rtn2, IPOINT_AFTER, (AFUNPTR)VQAfter,
-			IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
-		RTN_Close(rtn2);
+	TraceFile << "Return value of  CoTaskMemAlloc :" << CTMAlloc << " \n";
+}
+VOID GAfter(ADDRINT ret, IMG img) {
+	GAlloc = ret;
+	int todo = 1;
+	W::MEMORY_BASIC_INFORMATION memInfo;
+	if (img_counter < 100) {// still not working, i have to figure out how to add regions once while checking the whole array
+		for (int i = 0; i < img_counter; i++) {
+			if ((int)ret < mem_array[i].high && (int)ret >= mem_array[i].low) {
+				todo = 0;
+				break;
+			}
+		}
+		if (todo) {
+			W::VirtualQuery((W::LPCVOID)ret, &memInfo, sizeof(memInfo));
+			int mem_reg = (int)memInfo.BaseAddress + memInfo.RegionSize;
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].id = img_counter;
+			mem_array[img_counter].high = mem_reg - 1;
+			mem_array[img_counter].low = (int)memInfo.BaseAddress;
+			mem_array[img_counter].name = "GlobalAllocResult";
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].pagesType = memInfo.Type;
+			mem_array[img_counter].unloaded = 0;
+			img_counter++;
+		}
+	}
+	TraceFile << "Return value of  GlobalAlloc :" << GAlloc << " \n";
+}
+VOID HAAfter(ADDRINT ret, IMG img) {
+	HAlloc = ret;
+	int todo = 1;
+	W::MEMORY_BASIC_INFORMATION memInfo;
+	if (img_counter < 100) {// still not working, i have to figure out how to add regions once while checking the whole array
+		for (int i = 0; i < img_counter; i++) {
+			if ((int)ret < mem_array[i].high && (int)ret >= mem_array[i].low) {
+				todo = 0;
+				break;
+			}
+		}
+		if (todo) {
+			W::VirtualQuery((W::LPCVOID)ret, &memInfo, sizeof(memInfo));
+			int mem_reg = (int)memInfo.BaseAddress + memInfo.RegionSize;
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].id = img_counter;
+			mem_array[img_counter].high = mem_reg - 1;
+			mem_array[img_counter].low = (int)memInfo.BaseAddress;
+			mem_array[img_counter].name = "HeapAllocResult";
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].pagesType = memInfo.Type;
+			mem_array[img_counter].unloaded = 0;
+			img_counter++;
+		}
+	}
+	TraceFile << "Return value of  HeapAlloc :" << HAlloc << " \n";
+}
+VOID LAAfter(ADDRINT ret, IMG img) {
+	LAlloc = ret;
+	int todo = 1;
+	W::MEMORY_BASIC_INFORMATION memInfo;
+	if (img_counter < 100) {// still not working, i have to figure out how to add regions once while checking the whole array
+		for (int i = 0; i < img_counter; i++) {
+			if ((int)ret < mem_array[i].high && (int)ret >= mem_array[i].low) {
+				todo = 0;
+				break;
+			}
+		}
+		if (todo) {
+			W::VirtualQuery((W::LPCVOID)ret, &memInfo, sizeof(memInfo));
+			int mem_reg = (int)memInfo.BaseAddress + memInfo.RegionSize;
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].id = img_counter;
+			mem_array[img_counter].high = mem_reg - 1;
+			mem_array[img_counter].low = (int)memInfo.BaseAddress;
+			mem_array[img_counter].name = "LocalAllocResult";
+			mem_array[img_counter].protection = memInfo.Protect;
+			mem_array[img_counter].pagesType = memInfo.Type;
+			mem_array[img_counter].unloaded = 0;
+			img_counter++;
+		}
+	}
+	TraceFile << "Return value of  LocalAlloc :" << LAlloc << " \n";
+}
+VOID MAAfter(ADDRINT ret, VOID*  rtn) {
+	int todo = 1;
+	W::MEMORY_BASIC_INFORMATION memInfo;
+	mAlloc = ret;
+	if (img_counter < 100) {// still not working, i have to figure out how to add regions once while checking the whole array
+		for (int i = 0; i < img_counter; i++) {
+			if ((int)ret < mem_array[i].high && (int)ret >= mem_array[i].low) {
+				todo = 0;
+				break;
+			}
+		}
+		if (todo) {
+		W::VirtualQuery((W::LPCVOID)ret, &memInfo, sizeof(memInfo));
+		int mem_reg = (int)memInfo.BaseAddress + memInfo.RegionSize;
+		mem_array[img_counter].protection = memInfo.Protect;
+		mem_array[img_counter].id = img_counter;
+		mem_array[img_counter].high = mem_reg - 1;
+		mem_array[img_counter].low = (int)memInfo.BaseAddress;
+		mem_array[img_counter].name = "MallocResult";
+		mem_array[img_counter].protection = memInfo.Protect;
+		mem_array[img_counter].pagesType = memInfo.Type;
+		mem_array[img_counter].unloaded = 0;
+		TraceFile << "Base address: " << mem_array[img_counter].low << " High address: " << mem_array[img_counter].high << " \n";
+		TraceFile << "Return value of  malloc :" << mAlloc << " \n";
+		TraceFile << "Address of function: " << rtn << " \n";
+		img_counter++;
+		}
 	}
 }
-
+VOID VAAfter(ADDRINT ret, IMG img) {
+	VAlloc = ret;
+	TraceFile << " return value of  VirtualAlloc :" << VAlloc << " \n";
+}
 VOID MemAlloc(IMG img, VOID *v) {
 	//CoTaskMemAlloc  GlobalAlloc   HeapAlloc   LocalAlloc	malloc		new   VirtualAlloc
-	const char* names[] = { "VirtualQuery", "VirtualQueryEx", "CoTaskMemAlloc", "GlobalAlloc", "HeapAlloc", "LocalAlloc", "malloc", "new", "VirtualAlloc" };
-	
+	//const char* names[] = { "VirtualQuery", "VirtualQueryEx", "CoTaskMemAlloc", "GlobalAlloc", "HeapAlloc","LocalAlloc", "malloc", "new", "VirtualAlloc" };
+	fMap.insert(std::pair<std::string, int>("VirtualQuery", VirtualQuery_INDEX));
+	fMap.insert(std::pair<std::string, int>("VirtualQueryEx", VirtualQueryEx_INDEX));
+	fMap.insert(std::pair<std::string, int>("CoTaskMemAlloc", CoTaskMemAlloc_INDEX));
+	fMap.insert(std::pair<std::string, int>("GlobalAlloc", GlobalAlloc_INDEX));
+	fMap.insert(std::pair<std::string, int>("HeapAlloc", HeapAlloc_INDEX));
+	fMap.insert(std::pair<std::string, int>("LocalAlloc", LocalAlloc_INDEX));
+	fMap.insert(std::pair<std::string, int>("malloc", malloc_INDEX));
+	fMap.insert(std::pair<std::string, int>("new", new_INDEX)); // wierd
+	fMap.insert(std::pair<std::string, int>("VirtualAlloc", VirtualAlloc_INDEX));
+
+
+	for (std::map<string, int>::iterator it = fMap.begin(),
+		end = fMap.end(); it != end; ++it) {
+		const char* func_name = it->first.c_str();
+		RTN rtn = RTN_FindByName(img, func_name); // get pointer to the function
+		if (rtn != RTN_Invalid()) {
+			int index = it->second;
+			switch (index) {
+			case(VirtualQuery_INDEX):
+				if (RTN_Valid(rtn)) {
+					RTN_Open(rtn);
+					//function to parse VirtualQuery arguments
+					RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)ArgVQ,
+						IARG_ADDRINT, "VirtualQuery",
+						IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_FUNCARG_ENTRYPOINT_VALUE, 1, IARG_FUNCARG_ENTRYPOINT_VALUE, 2,
+						IARG_END);
+					//function to retrive VirtualQuery return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)VQAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+			case(VirtualQueryEx_INDEX):
+				if (RTN_Valid(rtn)) {// does not work properly need to reconfigure for VQEx using function for VQ
+					RTN_Open(rtn);
+					//function to parse VirtualQueryEx arguments
+					RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)ArgVQEx,
+						IARG_ADDRINT, "VirtualQueryEx",
+						IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_FUNCARG_ENTRYPOINT_VALUE, 1, IARG_FUNCARG_ENTRYPOINT_VALUE, 2,
+						IARG_END);
+					//function to retrive VirtualQuery return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)VQAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+			case(CoTaskMemAlloc_INDEX):
+				if (RTN_Valid(rtn)) {
+					RTN_Open(rtn);
+					//function to retrive CoTaskMemAlloc  return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)CTMAAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+			case(GlobalAlloc_INDEX):
+				if (RTN_Valid(rtn)) {
+					RTN_Open(rtn);
+					//function to retrive GlobalAlloc  return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)GAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+			case(HeapAlloc_INDEX):
+				if (RTN_Valid(rtn)) {
+					RTN_Open(rtn);
+					//function to retrive HeapAlloc  return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)HAAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+			case(LocalAlloc_INDEX):
+				if (RTN_Valid(rtn)) {
+					RTN_Open(rtn);
+					//function to retrive LocalAlloc  return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)LAAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+			case(malloc_INDEX):
+				if (RTN_Valid(rtn)) {
+					RTN_Open(rtn);
+					//function to retrive malloc  return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)MAAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_PTR , rtn, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+			//case(new_INDEX):
+				//do stuff
+				//break;
+			case(VirtualAlloc_INDEX):
+				if (RTN_Valid(rtn)) {
+					RTN_Open(rtn);
+					//function to retrive VirtualAlloc  return value	
+					RTN_InsertCall(rtn, IPOINT_AFTER, (AFUNPTR)VAAfter,
+						IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+					RTN_Close(rtn);
+				}
+				break;
+
+			}
+		}
+	}
 }
 
 VOID parse_funcsyms(IMG img, VOID *v) {
 	if (!IMG_Valid(img)) return;
 	W::MEMORY_BASIC_INFORMATION memInfo;
 	//building up an array in which i store valuable informations about the images
-	mem_array[img_counter].id = IMG_Id(img)-1;
+	mem_array[img_counter].id = IMG_Id(img) - 1;
 	mem_array[img_counter].high = IMG_HighAddress(img);
 	mem_array[img_counter].low = IMG_LowAddress(img);
 	mem_array[img_counter].name = IMG_Name(img);
@@ -302,12 +529,13 @@ VOID parse_funcsyms(IMG img, VOID *v) {
 	MemAlloc(img, 0);
 }
 
-VOID ImageUnload(IMG img, VOID* v){
+VOID ImageUnload(IMG img, VOID* v) {
 	int index = 0;
 	for (int i = 0; i < img_counter; i++) {
-		if (IMG_Id(img) == mem_array[i].id) {
+		if (IMG_Id(img)-1 == mem_array[i].id) {
 			mem_array[i].unloaded = 1;
 			index = i;
+			TraceFile << "img name: " << mem_array[i].name << " img ID: " << mem_array[i].id << " \n";
 		}
 	}
 	TraceFile << "img: " << mem_array[index].name << " is unloaded  \n";
